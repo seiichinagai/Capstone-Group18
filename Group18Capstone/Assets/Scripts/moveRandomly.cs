7ad64e2aac5b1c16e83d4331f7eefb83fe111405
Nergal Gibvarkes <ngivarke@asu.edu>
2018-11-22 21:24:18 -0800

    Adding random route to Fox

---
+++ Group18Capstone/Assets/Scripts/moveRandomly.cs
@@ -0,0 +1,37 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+ 
+ public class moveRandomly : MonoBehaviour {
+     private float timeToChangeDirection;
+  Rigidbody2D rigidbody;
+     // Use this for initialization
+     public void Start () {
+         ChangeDirection();
+          rigidbody = GetComponent<Rigidbody2D>();
+        
+     }
+     
+     // Update is called once per frame
+     public void Update () {
+         timeToChangeDirection -= Time.deltaTime;
+ 
+         if (timeToChangeDirection <= 0) {
+             ChangeDirection();
+         }
+ 
+         rigidbody.velocity = transform.up * 2;
+     }
+ 
+ 
+ 
+     private void ChangeDirection() {
+         float angle = Random.Range(0f, 360f);
+         Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+         Vector3 newUp = quat * Vector3.up;
+         newUp.z = 0;
+         newUp.Normalize();
+         transform.up = newUp;
+         timeToChangeDirection = 1.5f;
+     }
+ }
