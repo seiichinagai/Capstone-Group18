3e33f9032e1bda3bb4658742d13af65d891d675b
Nergal <ngivarke@asu.edu>
2019-01-22 16:41:04 -0800

    Second camera will switch using timer

--- Group18Capstone/Assets/Scripts/cameraSwitch.cs
+++ Group18Capstone/Assets/Scripts/cameraSwitch.cs
@@ -1,19 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class cameraSwitch : MonoBehaviour {
 
-   public bool SwitchCamera = false;
-    public Camera secondCamera;
-    public KeyCode pressedKey;
-    
 
-    void Update()
+    public class cameraSwitch : MonoBehaviour
     {
-        if (Input.GetKeyDown(pressedKey)){
+
+        float currentTime = 0f;
+        float startTime = 1f;
+        public bool SwitchCamera = false;
+        public Camera secondCamera;
+        public KeyCode pressedKey;
+        System.Timers.Timer time;
+
+     void Start()
+    {
+        currentTime = startTime;
+    }
+    void Update()
+        {
+       
+        currentTime += 1 * Time.deltaTime;
+        
+        Debug.Log((int) currentTime );
+
+        if ((int)currentTime == 27){
             SwitchCamera = !SwitchCamera;
             secondCamera.gameObject.SetActive(SwitchCamera);
+            Camera.main.enabled = false;
         }
-    }
+        }
+    
+       
+       
+        
+        
+    
+
+  
+ 
+
+
+
 }
+
+    
