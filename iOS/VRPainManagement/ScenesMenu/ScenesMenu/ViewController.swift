7d6bb7500226343ae6beb95fe1698ce1b80c6ea6
Nolan Miller <nolanrm20@gmail.com>
2019-02-02 00:13:39 -0700

    Base scenes page
    Also fixed spacing on the home screen (added connections to keep
    spacing consistent)

---
+++ iOS/VRPainManagement/ScenesMenu/ScenesMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScenesMenu
+//
+//  Created by Nolan Miller on 2/1/19.
+//  Copyright © 2019 Group18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
