4ec4d0471a3fefb2be8bbb3a9d37f38167117dfb
Nergal <ngivarke@asu.edu>
2019-02-07 20:27:43 -0800

    Update is added

--- Group18Capstone/Assets/Scripts/PlayerFollow.cs
+++ Group18Capstone/Assets/Scripts/PlayerFollow.cs
@@ -11,15 +11,15 @@ public class PlayerFollow : MonoBehaviour {
     [Range(0.01f, 1.0f)]
     public float SmoothFactor = 0.5f;
     public bool LookAtPlayer = false;
+    public bool zoom = false;
     float currentTime = 0f;
     float startTime = 1f;
     float xx;
 
     void Start () {
-        //bedroomView = new Vector3(210f, 20f, 80f);
-
+        camOff = new Vector3(0f, 40f, 200f);
         currentTime = startTime;
-        camOff = transform.position - player.position;	
+       // camOff = transform.position - player.position;	
 	}
 	
 	
@@ -30,23 +30,18 @@ public class PlayerFollow : MonoBehaviour {
         currentTime += 1 * Time.deltaTime;
         if ((int)currentTime == 30 )
         {
-           
-            Camera.main.transform.position = new Vector3(210f, 20f, 80f); ;
-          //Camera.main.transform.rotation = Quaternion.Euler(30, 30, 0);
-         // mainCam.fieldOfView = 13;
+           Camera.main.transform.position = new Vector3(210f, 20f, 80f); ;
+           //Camera.current.transform.rotation = Quaternion.Euler(30, 30, 0);
         }
 
         if ((int)currentTime == 27 || (int)currentTime >= 27)
         {
-            
-
-
             Vector3 newPos = player.position + camOff;
             transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
            
             if (LookAtPlayer)
             transform.LookAt(player);
-        }
+}
     }
            
         
