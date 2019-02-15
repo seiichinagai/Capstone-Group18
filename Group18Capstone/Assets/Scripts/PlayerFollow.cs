fde96cd94e2c6b53614c6d6996f00c58c09ebe14
Nergal <ngivarke@asu.edu>
2019-02-14 22:12:00 -0800

    View angle switches back and forth between two penguin and deer

--- Group18Capstone/Assets/Scripts/PlayerFollow.cs
+++ Group18Capstone/Assets/Scripts/PlayerFollow.cs
@@ -7,6 +7,7 @@ public class PlayerFollow : MonoBehaviour {
     private Vector3 bedroomView;
     Camera mainCam;
     public Transform player;
+    public Transform player2;
     private Vector3 camOff;
     [Range(0.01f, 1.0f)]
     public float SmoothFactor = 0.5f;
@@ -14,8 +15,8 @@ public class PlayerFollow : MonoBehaviour {
     public bool zoom = false;
     float currentTime = 0f;
     float startTime = 1f;
-    float xx;
-
+    public readonly float _TIME = 27;
+    public readonly float _TIME2 = 47;
     void Start () {
         camOff = new Vector3(0f, 15f, 80f);
         currentTime = startTime;
@@ -28,20 +29,36 @@ public class PlayerFollow : MonoBehaviour {
      
         
         currentTime += 1 * Time.deltaTime;
-        if ((int)currentTime == 30 )
+        Debug.Log("----------------------------------------"+(int) currentTime);
+        if ((int)currentTime == 28)
         {
-           Camera.main.transform.position = new Vector3(210f, 20f, 80f); ;
-           //Camera.current.transform.rotation = Quaternion.Euler(30, 30, 0);
+            Camera.main.transform.position = new Vector3(210f, 20f, 80f); ;
+            //Camera.current.transform.rotation = Quaternion.Euler(30, 30, 0);
         }
+       
 
-        if ((int)currentTime == 27 || (int)currentTime >= 27)
+        if ((int)currentTime >= _TIME  && !((int)currentTime > _TIME2))
         {
             Vector3 newPos = player.position + camOff;
             transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
            
             if (LookAtPlayer)
             transform.LookAt(player);
-}
+        }
+
+
+        if ((int)currentTime > _TIME2)
+        {
+            Vector3 newPos = player2.position + camOff;
+            transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+            if (LookAtPlayer)
+                transform.LookAt(player2);
+
+            if ((int)currentTime == 67) 
+                currentTime = 29;
+        }
+
     }
            
         
