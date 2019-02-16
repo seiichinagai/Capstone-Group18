e19f765a69df50e78934757aa372345f343465af
Sam Wells <sdwells4@asu.edu>
2019-02-16 18:17:47 -0500

    Camera Follow script testing code (currently commented out while in progress)

--- Group18Capstone/Assets/Scripts/PlayerFollow.cs
+++ Group18Capstone/Assets/Scripts/PlayerFollow.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFollow : MonoBehaviour {
+public class PlayerFollow : MonoBehaviour
+{
 
     private Vector3 bedroomView;
     Camera mainCam;
@@ -15,40 +16,64 @@ public class PlayerFollow : MonoBehaviour {
     float startTime = 1f;
     float xx;
 
-    void Start () {
+    void Start()
+    {
         //bedroomView = new Vector3(210f, 20f, 80f);
 
         currentTime = startTime;
-        camOff = transform.position - player.position;	
-	}
-	
-	
-	void Update () {
+        camOff = transform.position - player.position;
+    }
 
-     
-        
+
+    void Update()
+    {
+
         currentTime += 1 * Time.deltaTime;
-        if ((int)currentTime == 30 )
+        if ((int)currentTime == 30)
         {
-           
+
             Camera.main.transform.position = new Vector3(210f, 20f, 80f); ;
-          //Camera.main.transform.rotation = Quaternion.Euler(30, 30, 0);
-         // mainCam.fieldOfView = 13;
+            //Camera.main.transform.rotation = Quaternion.Euler(30, 30, 0);
+            // mainCam.fieldOfView = 13;
         }
 
         if ((int)currentTime == 27 || (int)currentTime >= 27)
         {
-            
-
 
             Vector3 newPos = player.position + camOff;
             transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
-           
+
             if (LookAtPlayer)
-            transform.LookAt(player);
+                transform.LookAt(player);
         }
     }
-           
-        
-       
+
+    /*
+     * Script modifications and testing
+     */
+    /*
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    float currentTime = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTime = 1f;
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        currentTime += 1 * Time.deltaTime;
+        if ((int)currentTime >= 27)
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+            transform.position = player.transform.position + offset;
+    }
+    */
+
 }
