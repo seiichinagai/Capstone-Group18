fc93999d8d4088c7ca08bcac3fba5f920d9e774b
Nolan Miller <nolanrm20@gmail.com>
2019-03-10 16:49:19 -0700

    Added generic route script
    There is a simple route (7 stops) created for a character. It’s
    attached to the penguin I tested on, but not the main character yet.

---
+++ Group18Capstone/Assets/Scripts/MainCharRoute.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MainCharRoute : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = true;
+
+            TravelToNext();
+        
+    }
+
+    void TravelToNext()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            TravelToNext();
+    }
+}
