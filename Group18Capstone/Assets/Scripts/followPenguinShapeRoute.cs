9216fb7970dd12c83626e03cb689c14d18b14fee
Nergal <ngivarke@asu.edu>
2019-04-05 11:21:51 -0700

    Now animals can follow a custome path on the penguin-shaped lake

---
+++ Group18Capstone/Assets/Scripts/followPenguinShapeRoute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPenguinShapeRoute : MonoBehaviour
+{
+
+    Vector3 last_position;
+    Vector3 current_position;
+    private float distanceToPath = 1.0f;
+    public float speedOfRot = 5.0f;
+    public string pathName;
+    public penguinShape pShaped;
+    public int ID = 0;
+    public float objectSpeed;
+   
+    void Start()
+    {
+      
+        last_position = transform.position;
+    }
+
+   
+    void Update()
+    {
+        float dis = Vector3.Distance(pShaped.routeTargets[ID].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, ps.routeTargets[ID].position, Time.deltaTime * objectSpeed );
+
+        var rotation = Quaternion.LookRotation(pShaped.routeTargets[ID].position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speedOfRot);
+
+        if (dis <= distanceToPath) {
+            ID++;
+        }
+        if (ID >= pShaped.routeTargets.Count) {
+            ID = 0; 
+        }
+    }
+}
