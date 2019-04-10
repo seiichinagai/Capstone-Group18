c86de8ba6e5c6d262b6f825853585f1346f06326
Nergal <ngivarke@asu.edu>
2019-04-10 10:17:15 -0700

    Fixing the conlict on task 128 and implementing the route for penguin 10

---
+++ Group18Capstone/Assets/Scripts/followPenguinShapeRoute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPenguinShapeRoute : MonoBehaviour
+{
+   
+   public penguinShapeAnimalRoute pShaped;
+    private float distanceToPath = 1.0f;
+    public float speedOfRot = 5.0f;
+    public string pathName;
+    
+    public int ID = 0;
+    public float objectSpeed;
+    Vector3 last_position;
+    Vector3 current_position;
+    void Start()
+    {
+         // pShaped = GameObject.Find(pathName).GetComponent<penguinShapeAnimalRoute>();
+        last_position = transform.position;
+    }
+
+   
+    void Update()
+    {
+        float dis = Vector3.Distance(pShaped.routeTargets[ID].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, pShaped.routeTargets[ID].position, Time.deltaTime * objectSpeed );
+
+        var rotation = Quaternion.LookRotation(pShaped.routeTargets[ID].position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speedOfRot);
+
+        if (dis <= distanceToPath) {
+            ID++;
+        }
+        if (ID >= pShaped.routeTargets.Count) {
+            ID = 0; 
+        }
+    }
+}
